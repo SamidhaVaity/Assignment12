4c2b7bfaa834528804324d54193fad205b91f21f
Samidha Vaity <samidhavaity@gmail.com>
2024-01-13 23:28:15 +0530

    Updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { PersonalInfoComponent } from './personal-info/personal-info.component';
+import { BugInfoComponent } from './bug-info/bug-info.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,HeaderComponent,PersonalInfoComponent,BugInfoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
